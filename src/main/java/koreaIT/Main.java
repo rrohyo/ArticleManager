31048b78fe3b04d7a9f108e3015179ad94b2825f
rrohyo <aoslwjs2926@gmail.com>
2025-04-16 19:55:43 +0900

    추가 기능, 목록 기능 구현

--- src/main/java/koreaIT/Main.java
+++ src/main/java/koreaIT/Main.java
@@ -1,11 +1,14 @@
 package koreaIT;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        List<Article> articleList = new ArrayList<>();
 
         System.out.println(" == 프로그램 시작 == ");
 
@@ -14,18 +17,33 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.println("명령어");
+            System.out.printf("명령어) ");
             String cmd = sc.nextLine();
 
             if (cmd.equals("article list")) {
                 System.out.println("번호  /  제목  /  내용");
+                for (int i = articleList.size()-1 ; i >= 0 ; i--) {
+                    System.out.printf("%d  /%s  /%s\n", articleList.get(i).getId(),
+                            articleList.get(i).getTitle(),
+                            articleList.get(i).getBody());
+
+                }
             } else if (cmd.equals("article write")) {
-                System.out.println("제목 : ");
+                System.out.printf("제목 : ");
                 String title = sc.nextLine();
-                System.out.println("내용 : ");
+                System.out.printf("내용 : ");
                 String body = sc.nextLine();
 
                 lastId++;
+                int id = lastId;
+
+                Article addArticle = new Article();
+                addArticle.setId(id);
+                addArticle.setTitle(title);
+                addArticle.setBody(body);
+
+                articleList.add(addArticle);
+
                 System.out.printf("%d번 글이 생성되었습니다.\n", lastId);
             } else if (cmd.equals("exit")) {
                 System.out.println(" == 프로그램 종료 == ");
