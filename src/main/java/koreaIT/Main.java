089c54a907daa974ff2a37b43fe1b0c5b09a20e9
rrohyo <aoslwjs2926@gmail.com>
2025-04-16 19:33:37 +0900

    게시글 작성 기능 피상적 구현

--- src/main/java/koreaIT/Main.java
+++ src/main/java/koreaIT/Main.java
@@ -9,17 +9,33 @@ public class Main {
 
         System.out.println(" == 프로그램 시작 == ");
 
+        int lastId = 0;
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("명령어");
-        String cmd = sc.nextLine();
 
-        System.out.println(cmd);
+        while (true) {
+            System.out.println("명령어");
+            String cmd = sc.nextLine();
 
-        if(cmd.equals("exit")) {
-            System.out.println(" == 프로그램 종료 == ");
-        }
+            if (cmd.equals("article list")) {
+                System.out.println("번호  /  제목  /  내용");
+            } else if (cmd.equals("article write")) {
+                System.out.println("제목 : ");
+                String title = sc.nextLine();
+                System.out.println("내용 : ");
+                String body = sc.nextLine();
 
+                lastId++;
+                System.out.printf("%d번 글이 생성되었습니다.\n", lastId);
+            } else if (cmd.equals("exit")) {
+                System.out.println(" == 프로그램 종료 == ");
+                break;
+            }
+            else {
+                System.out.println("사용 할 수 없는 명령어 입니다.");
+            }
 
 
         }
     }
+}
