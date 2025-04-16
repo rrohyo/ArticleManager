156f68a6f42cb1d744e229006d95e446acb30f37
rrohyo <aoslwjs2926@gmail.com>
2025-04-16 20:47:39 +0900

    상세보기할 게시글 찾는 기능 구현 중

--- src/main/java/koreaIT/Main.java
+++ src/main/java/koreaIT/Main.java
@@ -20,23 +20,33 @@ public class Main {
             System.out.printf("명령어) ");
             String cmd = sc.nextLine().trim();
 
-            if(cmd.startsWith("article detail")){
+            if(cmd.startsWith("article detail")) {
+                // parsing start
                 String[] cmdBits = cmd.split(" ");
 
-                if(cmdBits.length > 3) {
+                if (cmdBits.length > 3) {
                     System.out.println("명령어를 제대로 입력해주세요");
                 }
-
-                try{
-                    int detailId = Integer.parseInt(cmdBits[2]);
-                }catch (NumberFormatException e) {
+                int detailId = -1;
+                try {
+                    detailId = Integer.parseInt(cmdBits[2]);
+                } catch (NumberFormatException e) {
                     System.out.println("정수를 제대로 입력해 주세요");
-                }catch (ArrayIndexOutOfBoundsException e) {
+                } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.println("정수를 추가해서 입력해주세요");
                 }
-
-
-
+                // parsing end
+                if (!articleList.isEmpty()) {
+                    for (Article article : articleList) {
+                        if (article.getId() == detailId) {
+                            System.out.println("너가 찾는 게시물 있어");
+                        } else {
+                            System.out.printf("%d번 게시물은 없습니다\n", detailId);
+                        }
+                    }
+                } else {
+                    System.out.println("게시글이 없습니다");
+                }
             }
 
             else if (cmd.equals("article list")) {
